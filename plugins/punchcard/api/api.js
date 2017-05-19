811f28c3385296885e4753430ed52e3d3c2edf0d
aeroith <sc@count.ly>
2017-05-19 14:06:34 +0000

    Fix delete events

--- plugins/punchcard/api/api.js
+++ plugins/punchcard/api/api.js
@@ -6,21 +6,22 @@ const plugin = {}
 (function (plugin) {
     plugins.register("/i", function(ob) {
         var params = ob.params;
+		console.log(params);
         // time of day parameter
         var dow_hour = {};
         if(params.qstring.begin_session || params.qstring.events) {
             const events = params.qstring.events;
-            const app_key = params.qstring.app_key;
+            const app_id = params.app_id;
             const field = params.qstring.dow + "-" + params.qstring.hour;
             // increase one every time
             dow_hour[field] = 1;
             events.forEach((e) => {
                 common.db.collection('timesofday').update({
-                    _id: common.md5Hash(app_key + e.key)
+                    _id: common.md5Hash(app_id + e.key)
                 },
                 {
                     $set: {
-                        app_id: app_key,
+                        app_id: app_id,
                         e: e.key,
                     },
                     $inc: dow_hour
@@ -30,7 +31,28 @@ const plugin = {}
             return true;
         }
         return false;
-    })
+    });
+   plugins.register("/i/apps/delete", function(ob){
+		var appId = ob.appId;
+		common.db.collection('timesofday').remove({'app_id': common.db.ObjectID(appId)},function(){});
+	});
+	
+	plugins.register("/i/apps/reset", function(ob){
+		var appId = ob.appId;
+		common.db.collection('timesofday').remove({'app_id': common.db.ObjectID(appId)},function(){});
+	});
+    
+    plugins.register("/i/apps/clear_all", function(ob){
+        var appId = ob.appId;
+		common.db.collection('timesofday').remove({'app_id': common.db.ObjectID(appId)},function(){});
+    });
+    
+    plugins.register("/i/apps/clear", function(ob){
+		var appId = ob.appId;
+        var ids = ob.ids;
+		common.db.collection('timesofday').remove({$and:[{'app_id': common.db.ObjectID(appId)}, {'app_id': {$nin:ids}}]},function(){});
+	});
+ 
 }(plugin));
 
 module.exports = plugin;
